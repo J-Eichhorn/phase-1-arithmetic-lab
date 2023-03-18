5face696b7da5d6f54e522db92c88477cb4c1ba7
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-03-18 15:27:45 -0400

    does math

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 4;
+let num4 = 16;
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(1,20);
